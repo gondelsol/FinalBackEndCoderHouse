9848053b349e4b3c74608d1f34c1574b99a5aa62
gondelsol <gondelsol@gmail.com>
2022-12-30 16:46:41 -0300

    arreglos en routes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,24 +1,28 @@
 import express from "express";
-import { tipeOfClient } from '../index.js';
-import {getProducts, postProducts, putProducts, deleteProducts} from "../controllers/products/productsHandlers.js";
-import { validId, existsProduct } from "../controllers/products/productsValidations.js";
 import Container from "../Classes/class.js";
-
-//const tipeOfClient = false;
 
 const {Router} = express;
 const productRouter = Router();
 
+import { isAdmin } from "../controllers/validation.js";
+import {getProducts, postProducts, putProducts, deleteProducts} from "../controllers/products/productsHandlers.js";
+import { validId, existsProduct } from "../controllers/products/productsValidations.js";
 
-export const prodContainer = new Container( "../files/products.txt")
+
+//const tipeOfClient = false;
+
+
+
+
+export const prodContainer = new Container( '../files/products.txt')
 
 productRouter.get("/:id?", validId, getProducts);
 
-productRouter.post("/", tipeOfClient , postProducts);
+productRouter.post("/", isAdmin , postProducts);
 
-productRouter.put("/:id", tipeOfClient, existsProduct, putProducts);
+productRouter.put("/:id", isAdmin, existsProduct, putProducts);
 
-productRouter.delete("/:id", tipeOfClient, existsProduct, deleteProducts);
+productRouter.delete("/:id", isAdmin, existsProduct, deleteProducts);
 
 
 export default productRouter;
